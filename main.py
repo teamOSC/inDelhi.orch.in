7df52f41099fc2c6ee41ac2880f3d0e7db9a4372
sauravtom <stomatrix@gmail.com>
2015-01-14 18:40:22 +0530

    cleaned up some stuff

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import urllib2
 import urllib
 import json
 import logging
-
+import hashlib
 import jinja2
 import os
 import urlparse
@@ -14,7 +14,12 @@ import urlparse
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'])
-    
+
+def get_dict(**kwargs):
+    d= {}
+    for k,v in kwargs.iteritems():
+        d[k] = v
+    return d
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -27,13 +32,16 @@ class View(webapp2.RequestHandler):
         json_ob = json.loads(urllib2.urlopen(url).read())
         arr = []
         for i in json_ob["feed"]["entry"]:
+            d= {}
             title = i["gsx$title"]["$t"]
             description = i["gsx$description"]["$t"]
             image = i["gsx$image"]["$t"]
             time = i["gsx$time"]["$t"]
             venue = i["gsx$venue"]["$t"]
-            coordinates = i["gsx$coordinates"]["$t"]
-            arr.append([title,description,image,time,venue,coordinates])
+            tags = i["gsx$tags"]["$t"]
+            id = hashlib.md5(title,description,image).hexdigest()[:6]
+            d = get_dict(id=id,title=title,description=description,image=image,time=time,venue=venue,tags=tags)
+            arr.append(d)
 
         template = jinja_environment.get_template('templates/index.html')
         self.response.out.write(template.render(arr=arr))
